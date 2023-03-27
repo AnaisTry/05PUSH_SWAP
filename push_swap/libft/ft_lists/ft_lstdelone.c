c95c6306dec55f3dee2c707ba3baf0ed20f491c8
Anais Gassin <angassin@shi-r6-p6.s19.be>
2023-03-27 11:16:28 +0200

    libft, draft makefile

---
+++ push_swap/libft/ft_lists/ft_lstdelone.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angassin <angassin@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 13:52:48 by angassin          #+#    #+#             */
+/*   Updated: 2023/02/25 15:00:19 by angassin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+/*
+	Takes as a parameter a node and frees the memory of the node’s content 
+	using the function ’del’ given as a parameter and free the node. 
+	The memory of ’next’ must not be freed.
+*/
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
