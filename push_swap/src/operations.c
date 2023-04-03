52d873a910818656f1b553db999e68f5b966a53a
Anais Gassin <angassin@shi-r10-p6.s19.be>
2023-04-03 13:24:19 +0200

    created push

--- push_swap/src/operations.c
+++ push_swap/src/operations.c
@@ -6,13 +6,13 @@
 /*   By: angassin <angassin@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 13:02:50 by angassin          #+#    #+#             */
-/*   Updated: 2023/04/03 11:59:52 by angassin         ###   ########.fr       */
+/*   Updated: 2023/04/03 13:19:16 by angassin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	swap(t_node **stack, char name)
+void	swap(t_node **stack, char stack_name)
 {
 	t_node	*temp;
 
@@ -20,11 +20,11 @@ void	swap(t_node **stack, char name)
 	(*stack)->next = temp->next;
 	temp->next = *stack;
 	*stack = temp;
-	ft_printf("s%c\n", name);
+	ft_printf("s%c\n", stack_name);
 }
 
 /* First node becomes last  a b c -> b c a */
-void	rotate(t_node **stack, char name)
+void	rotate(t_node **stack, char stack_name)
 {
 	t_node	*first;
 
@@ -32,11 +32,11 @@ void	rotate(t_node **stack, char name)
 	*stack = (*stack)->next;
 	first->next = NULL;
 	node_add_back(stack, first);
-	ft_printf("r%c\n", name);
+	ft_printf("r%c\n", stack_name);
 }
 
 /* Last node becomes first a b c -> c a b */
-void	reverse_rotate(t_node **stack, char name)
+void	reverse_rotate(t_node **stack, char stack_name)
 {
 	t_node	*last;
 
@@ -46,13 +46,15 @@ void	reverse_rotate(t_node **stack, char name)
 	last->next->next = *stack;
 	*stack = last->next;
 	last->next = NULL;
-	ft_printf("rr%c\n", name);
+	ft_printf("rr%c\n", stack_name);
 }
 
-void	push(t_node **dest, t_node **src, char name)
+void	push(t_node **dest, t_node **src, char stack_name)
 {
 	t_node	*temp;
-	
+
 	temp = (*src)->next;
-	
+	node_add_front(dest, *src);
+	(*src) = temp;
+	ft_printf("p%c\n", stack_name);
 }
